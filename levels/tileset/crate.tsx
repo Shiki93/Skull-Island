7eba791bc3ed9b737579124f6d116303d2f42fbc
Otoniel Murillo <murillo.tec@gmail.com>
2023-10-07 21:25:15 -0600

    Se creo el nivel con las imagenes correspondientes

---
+++ levels/tileset/crate.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="crate" tilewidth="64" tileheight="42" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="58" height="42" source="../../graphics/terrain/crate.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="C:/Users/TOTO/Downloads/Treasure Hunters/Merchant Ship/Sprites/Chest/Idle/1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="35" source="C:/Users/TOTO/Downloads/Treasure Hunters/Palm Tree Island/Sprites/Objects/Chest/Chest Close 01.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="35" source="C:/Users/TOTO/Downloads/Treasure Hunters/Palm Tree Island/Sprites/Objects/Chest/Chest Open 07.png"/>
+ </tile>
+</tileset>
