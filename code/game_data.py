ebfd4cd2c1e4ec0ed961c76707efb52a9e69c3b4
Otoniel Murillo <murillo.tec@gmail.com>
2023-10-09 15:51:41 -0600

    Cambio en la estructura del codigo, se creo el terreno, pasto y las cajas

---
+++ code/game_data.py
@@ -0,0 +1,10 @@
+level_0 = {
+		'terrain': 'E:/Programacion/Skull Island/levels/0/level_0_terrain.csv',
+		'coins':'E:/Programacion/Skull Island/levels/0/level_0_coins.csv',
+		'fg palms':'E:/Programacion/Skull Island/levels/0/level_0_fg_palms.csv',
+		'bg palms':'E:/Programacion/Skull Island/levels/0/level_0_bg_palms.csv',
+		'crates': 'E:/Programacion/Skull Island/levels/0/level_0_crates.csv',
+		'enemies':'E:/Programacion/Skull Island/levels/0/level_0_enemies.csv',
+		'constraints':'E:/Programacion/Skull Island/levels/0/level_0_constraints.csv',
+		'player': 'E:/Programacion/Skull Island/levels/0/level_0_player.csv',
+		'grass': 'E:/Programacion/Skull Island/levels/0/level_0_grass.csv'}
---
+++ code/level.py
@@ -0,0 +1,57 @@
+import pygame
+from support import import_csv_layout, import_cut_graphics
+from settings import tile_size
+from tiles import Tile, StaticTile, Crate
+
+class Level:
+    def __init__(self, level_data, surface):
+        
+        #general setup
+        self.display_surface = surface
+        self.world_shift = -5
+
+        #terrain setup
+        terrain_layout = import_csv_layout(level_data['terrain'])
+        self.terrain_sprites = self.create_tile_group(terrain_layout, 'terrain')
+
+        #Grass setup
+        grass_layout = import_csv_layout(level_data['grass'])
+        self.grass_sprites = self.create_tile_group(grass_layout, 'grass')
+
+        #Crates setup
+        crate_layout = import_csv_layout(level_data['crates'])
+        self.crate_sprites = self.create_tile_group(crate_layout,'crates')
+    
+    def create_tile_group(self, layout, type):
+        sprite_group = pygame.sprite.Group()
+        for row_index, row in enumerate(layout):
+            for col_index, val in enumerate(row):
+                if val != '-1':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+                    if type == 'terrain':
+                        terrain_tile_list = import_cut_graphics('E:/Programacion/Skull Island/graphics/terrain/terrain_tiles.png')
+                        tile_surface = terrain_tile_list[int(val)]
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
+                    if type == 'grass':
+                        grass_tile_list = import_cut_graphics('E:/Programacion/Skull Island/graphics/decoration/grass/grass.png')
+                        tile_surface = grass_tile_list[int(val)]
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
+                    if type == 'crates':
+                        sprite = Crate(tile_size,x,y)
+                    sprite_group.add(sprite)
+        return sprite_group
+
+    def run(self):
+
+        #terrain
+        self.terrain_sprites.update(self.world_shift)
+        self.terrain_sprites.draw(self.display_surface)
+        
+        #grass
+        self.grass_sprites.update(self.world_shift)
+        self.grass_sprites.draw(self.display_surface)
+
+        #Crates
+        self.crate_sprites.update(self.world_shift)
+        self.crate_sprites.draw(self.display_surface)
